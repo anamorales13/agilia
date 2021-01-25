1179e6d1c397944179b6b65eb3bfb3540c061eb0
anamorales13 <maldonado.morales@alu.uhu.es>
2021-01-25 18:42:03 +0100

    delete user

--- src/components/Users.js
+++ src/components/Users.js
@@ -68,9 +68,9 @@ class Users extends Component {
 
     }
 
-    delete=(body)=>{
+    delete =(body)=>{
         console.log("body" + body);
-        axios.delete('https://reqres.in/api/users/' + body)
+       axios.delete('https://reqres.in/api/users/' + body)
         .then(res=>{
             this.setState({
                 status:'sucess'
@@ -117,7 +117,7 @@ class Users extends Component {
                                 <h3>{user.email}</h3>
 
                             </div>
-                            <button style={{background:'transparent', border:'none'}} onClick={this.delete(user._id)}>
+                            <button className="btn-delete" onClick={() => { if (window.confirm('\n' + '¿Estás seguro de eliminar el usuario ' + user.first_name + " " + user.last_name +"?")) this.delete(user.id); }}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
                                 <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
@@ -140,9 +140,7 @@ class Users extends Component {
                     {listarusers}
 
                 </div>
-                <div>
-                    <Link to="/">NUEVO</Link>
-                </div>
+                
                 {paginationElement}
 
             </div>
